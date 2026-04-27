d7d9db461240ac52aa8d66363dc8541ad11efda0
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2026-04-27 15:52:51 -0300

    style: visualização da home para usuário

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,8 @@ const userController = require('../controllers/userController');
 router.get('/login', userController.getLogin);
 router.post('/login', userController.postLogin);
 router.get('/home', userController.getHome);
+router.get('/produtos', userController.getProdutos);
+router.get('/avaliacao-ia', userController.getAvaliacaoIA);
 router.post('/logout', userController.postLogout);
 
 module.exports = router;
